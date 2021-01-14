a94a52d2f72763dcc1d03f4a45e646541071f267
jangelo99 <james.j.angelo@leidos.com>
2021-01-14 14:44:06 -0500

    Implemented matrix multiplication

--- python/ray_tracer/matrix.py
+++ python/ray_tracer/matrix.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from tuple import Tuple, Point, Vector
+
 
 class Matrix:
 
@@ -11,3 +13,14 @@ class Matrix:
 
   def __eq__(self, matrix):
     return np.array_equal(self.data, matrix.data)
+
+  def __mul__(self, other):
+    if isinstance(other, Matrix):
+      return Matrix(np.matmul(self.data, other.data))
+    elif isinstance(other, Tuple):
+      b = np.array([other.x, other.y, other.z, other.w])
+      result = np.matmul(self.data, b)
+      if other.w == 1:
+        return Point(result[0], result[1], result[2])
+      else:
+        return Vector(result[0], result[1], result[2])
--- python/ray_tracer/matrix_tests.py
+++ python/ray_tracer/matrix_tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from matrix import Matrix
+from tuple import Point, Vector
 
 class MatrixTestCase(unittest.TestCase):
 
@@ -26,3 +27,18 @@ class MatrixTestCase(unittest.TestCase):
     self.assertTrue(A == B)
     C = Matrix([[1, 2, 3, 4], [5.5, 6.5, 7.5, 8.5], [9, 10, 11, 12], [13.5, 14.5, 15.5, 16.5]])
     self.assertTrue(A != C)
+
+  def test_matrix_multiply(self):
+    # multiply matrix by matrix
+    A = Matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])
+    B = Matrix([[-2, 1, 2, 3], [3, 2, 1, -1], [4, 3, 6, 5], [1, 2, 7, 8]])
+    result = A * B
+    self.assertEqual(result, Matrix([[20, 22, 50, 48], [44, 54, 114, 108], [40, 58, 110, 102], [16, 26, 46, 42]]))
+    # multiply matrix by tuples
+    C = Matrix([[1, 2, 3, 4], [2, 4, 4, 2], [8, 6, 4, 1], [0, 0, 0, 1]])
+    d = Point(1, 2, 3)
+    result = C * d
+    self.assertTrue(result.equals(Point(18, 24, 33)))
+    d = Vector(1, 2, 3)
+    result = C * d
+    self.assertTrue(result.equals(Vector(14, 22, 32)))
